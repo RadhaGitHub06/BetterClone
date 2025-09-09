3a873516051d206f1fd8ae7b2ad78045ebec9fdb
Radha Gupta <rg9319738@gmail.com>
2025-09-09 13:24:30 +0530

    fix(ts): resolve JSX IntrinsicElements errors; include .d.ts in tsconfig

--- components/sections/HomeSection1.tsx
+++ components/sections/HomeSection1.tsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import Image from "next/image";
+
 
 const HomeSection1 = () => {
   return (
--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,10 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
+
+// Ensure JSX namespace is available to TS when using the app router
+declare namespace JSX {
+  interface IntrinsicElements {
+    [elemName: string]: any;
+  }
+}
